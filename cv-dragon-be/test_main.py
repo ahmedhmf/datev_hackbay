0e248b6aecb6708af900b54e4fb5b296a45691bd
AnjaWurli <anja.wurlitzer.coding@gmail.com>
2023-05-23 15:21:44 +0200

    Merge remote-tracking branch 'origin/main'

--- cv-dragon-be/test_main.py
+++ cv-dragon-be/test_main.py
@@ -12,3 +12,6 @@ def test_read_main():
 
     response = client.post("/uploadfile/", files={"file": ("filename", open(filename, "rb"), "multipart/formdata")})
     assert response.status_code == 200
+    assert response.json().get('page-content') is not None
+    assert response.json()["page-content"] == "PrintNode Multi \nPage Test Start\n1\n 2\n 3\n 4\n 5\n 6\n 7\n 8\n 9\n PrintNode \nMultipage Test End\n10\n"
+    
