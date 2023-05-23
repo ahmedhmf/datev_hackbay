cf9fcdee7c929775b274076efe83518acc74a2d5
Stefan Spangel <mail+github@stefanspangel.de>
2023-05-23 14:09:54 +0200

    Some small refactorings for Docs API Co-authored-by: dhohner <dhohner@users.noreply.github.com>

--- cv-dragon-be/main.py
+++ cv-dragon-be/main.py
@@ -5,24 +5,17 @@ from PyPDF2 import PdfReader
 
 app = FastAPI()
 
-
-@app.post("/files/")
-async def create_file(file: Annotated[bytes | None, File()] = None):
-    if not file:
-        return {"message": "No file sent"}
-    else:
-        return {"file_size": len(file)}
-
-
 def read_pdf_to_str(uploaded_file: UploadFile) -> str:
     reader = PdfReader(uploaded_file.file)
-    page = reader.pages[0]
-    print(page.extract_text())
+    page_strs = []
+    for page in reader.pages:
+        page_strs.append(page.extractText())
+    return " ".join(page_strs)
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile | None = None):
     if not file:
         return {"message": "No upload file sent"}
     else:
-        read_pdf_to_str(file)
-        return {"filename": file.filename}
+        page_content = read_pdf_to_str(file)
+        return {'page-content': page_content}
