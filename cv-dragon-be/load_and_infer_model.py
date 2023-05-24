67414571848077cfa1238835268e07bcd36d1307
Christiaan Swart <cswart@outlook.com>
2023-05-24 10:24:03 +0200

    fix import

--- cv-dragon-be/load_and_infer_model.py
+++ cv-dragon-be/load_and_infer_model.py
@@ -6,6 +6,31 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 
 
+# 0 ACCOUNTANT
+# 1 ADVOCATE
+# 2 AGRICULTURE
+# 3 APPAREL
+# 4 ARTS
+# 5 AUTOMOBILE
+# 6 AVIATION
+# 7 BANKING
+# 8 BPO
+# 9 BUSINESS-DEVELOPMENT
+# 10 CHEF
+# 11 CONSTRUCTION
+# 12 CONSULTANT
+# 13 DESIGNER
+# 14 DIGITAL-MEDIA
+# 15 ENGINEERING
+# 16 FINANCE
+# 17 FITNESS
+# 18 HEALTHCARE
+# 19 HR
+# 20 INFORMATION-TECHNOLOGY
+# 21 PUBLIC-RELATIONS
+# 22 SALES
+# 23 TEACHER
+
 model = load('basic_forest.joblib') 
 
 vect = load('tfidf_vect.joblib')
@@ -13,6 +38,16 @@ vect = load('tfidf_vect.joblib')
 def predict(texts: List[str]) -> np.ndarray:
     vectorised_text = vect.transform(texts)
     return model.predict_proba(vectorised_text)
+
+def predict_class(texts, class_of_interest):
+    if class_of_interest not in model.classes_:
+        print(f"{class_of_interest} not in {model.classes_}")
+    else:
+        class_idx = list(model.classes_).index(class_of_interest)
+        predictions = model.predict_proba(vectorised_text)
+        return list(predictions[:, class_idx])
+
+
 
 def class_info(model):
     return model.classes_
