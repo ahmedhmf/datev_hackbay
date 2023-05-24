e23740b29e121e4cfb2b629dc0fe72e526883317
Christiaan Swart <cswart@outlook.com>
2023-05-24 09:37:37 +0200

    fix

--- cv-dragon-be/load_and_infer_model.py
+++ cv-dragon-be/load_and_infer_model.py
@@ -1,4 +1,6 @@
 from joblib import dump, load
+import numpy as np
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 
@@ -7,6 +9,9 @@ model = load('filename.joblib')
 
 vect = load('tfidf_vect.joblib')
 
-def predict(text):
-    vectorised_text = vect.transform(text)
+def predict(texts: List[str]) -> np.ndarray:
+    vectorised_text = vect.transform(texts)
     return model.predict_proba(vectorised_text)
+
+def class_info(model):
+    return model.classes_
